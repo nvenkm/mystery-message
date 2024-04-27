41121e8ef50448f0a5bb488798481e96ffd689e8
naveen kumar <7naveennn@gmail.com>
2024-04-27 22:22:52 +0530

    Logo text and font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { User } from "next-auth";
 import { Button } from "./ui/button";
+import { Recursive } from "next/font/google";
+const recursive = Recursive({ subsets: ["cyrillic-ext"] });
 
 const Navbar = () => {
   const { data: session } = useSession();
@@ -13,9 +15,14 @@ const Navbar = () => {
   console.log("User::::::;;;;", session);
 
   return (
-    <nav className="p-3 md:p-5 md:px-20 shadow-sm">
+    <nav className="p-3 md:p-4 md:px-20 shadow-sm bg-purple-50">
       <div className="flex justify-between items-center">
-        <Link href="/">Mystery Message</Link>
+        <Link
+          className={`${recursive.className} text-gray-800 font-extrabold font-`}
+          href="/"
+        >
+          GET-Responses ✉️
+        </Link>
         {session ? (
           <>
             <span>Welcome, {user.username}</span>
