f9077b5207c5f50bb224b3f34264cf0bcf6aae6b
naveen kumar <7naveennn@gmail.com>
2024-05-06 23:05:44 +0530

    Added basic Homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,57 @@
-import Image from "next/image";
+"use client";
+import Autoplay from "embla-carousel-autoplay";
+import messages from "@/messages.json";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import React from "react";
 
 export default function Home() {
-  return <div>Hi</div>;
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+
+  return (
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dive into the World of Anonymous Feedback
+        </h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">
+          True Feedback - Where your identity remains a secret.
+        </p>
+      </section>
+      <Carousel plugins={[plugin.current]} className="w-[60vw]">
+        <CarouselContent>
+          {messages.map((message) => (
+            <CarouselItem key={message.title}>
+              <div className="p-1">
+                <Card>
+                  <CardHeader>{message.title}</CardHeader>
+                  <CardContent className="flex items-center p-6">
+                    <span className="text-4xl font-semibold">
+                      {message.content}
+                    </span>
+                  </CardContent>
+                  <CardFooter>{message.received}</CardFooter>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="text-accent-foreground" />
+        <CarouselNext className="text-accent-foreground" />
+      </Carousel>
+    </main>
+  );
 }
