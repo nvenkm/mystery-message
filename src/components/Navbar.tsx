bbbf462b5c9b49fc5e6793e9ca492543db0e6846
naveen kumar <7naveennn@gmail.com>
2024-05-21 17:36:54 +0530

    Font size fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     <nav className="p-2 md:px-20 shadow-sm backdrop-blur-lg bg-white/70 fixed w-full">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <Link
-          className={`${recursive.className} text-sm md:text:lg text-gray-800 font-semibold`}
+          className={`${recursive.className} text-sm md:text-lg text-gray-800 font-semibold`}
           href="/"
         >
           GET-Responses ✉️
