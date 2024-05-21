8b51a187ad8248c419f1f19473f116a0212f4f60
naveen kumar <7naveennn@gmail.com>
2024-05-21 11:55:09 +0530

    Change Page titles

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Dashboard - Get responses",
+  description: "Share your link and receive anonymous responses",
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
--- src/app/(u)/u/[...username]/layout.tsx
+++ src/app/(u)/u/[...username]/layout.tsx
@@ -7,22 +7,12 @@ import { Metadata } from "next";
 const recursive = Recursive({ subsets: ["cyrillic-ext"] });
 
 export const metadata: Metadata = {
-  title: "Signup or Signin to Get-Responses",
+  title: "Send Message - Get Responses ",
   description: "Create account or Signin",
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex flex-col justify-center items-start md:items-center min-h-screen bg-purple-100 px-4">
-      <Link
-        className={`${recursive.className} absolute top-0 left-0 m-4 text-sm md:text-lg text-gray-800 font-semibold`}
-        href="/"
-      >
-        GET-Responses ✉️
-      </Link>
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 };
 
 export default Layout;
