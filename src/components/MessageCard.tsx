eb9072d520a68f8ed26737d4d6333989f51dfb93
Naveen Kumar <7naveennn@gmail.com>
2024-07-06 10:12:30 +0530

    Message update and add env.example file

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -53,8 +53,8 @@ const MessageCard = ({ message, onMessageDelete }: MessageCardProps) => {
             <AlertDialogHeader>
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
+                This action cannot be undone. This will permanently delete this
+                message.
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
